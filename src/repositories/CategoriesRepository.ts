38bed92a64e2aff1114f3d8e84f83e27035e93cc
SnorlaxSPI <aflinsjr@icloud.com>
2024-07-28 11:08:35 -0300

    Validando o cadastro de categorias

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -28,7 +28,12 @@ class CategoriesRepository {
 
   list(): Category[] {
     return this.categories;
-  };
+  }
+
+  findByName(name: string): Category | void {
+    const category = this.categories.find(category => category.name === name);
+    return category;
+  }
 };
 
 export { CategoriesRepository };
